559e71a1848abe081cda2282b8ac0a847d3b01b8
manasbairwa9-ops <manasbairwa9@gmail.com>
2026-05-25 17:58:50 +0530

    Add files via upload

--- trpc.ts
+++ trpc.ts
@@ -1,22 +1,4 @@
-import { initTRPC } from '@trpc/server';
-import { Context } from './context';
-import { ZodError } from 'zod';
+import { createTRPCReact } from '@trpc/react-query';
+import type { AppRouter } from '@mehndi/api';
 
-const t = initTRPC.context<Context>().create({
-  errorFormatter({ shape, error }) {
-    return {
-      ...shape,
-      data: {
-        ...shape.data,
-        zodError:
-          error.cause instanceof ZodError
-            ? error.cause.flatten()
-            : null,
-      },
-    };
-  },
-});
-
-export const router = t.router;
-export const publicProcedure = t.procedure;
-export const middleware = t.middleware;
+export const trpc = createTRPCReact<AppRouter>();
